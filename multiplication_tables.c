75b97dac021120c544a556159a800360651444bd
1216945411 <1216945411@qq.com>
2016-10-26 21:03:11 +0800

    update

---
+++ multiplication_tables.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+	int x = 10;
+	int i=0,j=0;
+	for(i=1;i<=9;i++){
+		for(j=1;j<=i;j++){
+			sleep(1);
+			printf ("%d*%d=%d\t",j,i,i*j);
+			fflush(stdout);
+		}
+		printf("\n");
+	}
+	return 0;
+}
